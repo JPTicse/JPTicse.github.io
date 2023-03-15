ec5c0c7d6c32eb39a9c67002b31604e005614cc8
JPTicse <jpticse@gmail.com>
2023-03-15 08:43:24 -0500

    Fix typo

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'jpticse.github.io/'
+  base: 'jpticse.github.io/',
   plugins: [react()],
 })
